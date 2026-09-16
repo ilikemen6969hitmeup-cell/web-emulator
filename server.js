40e15e2874a68bfd3a12feb48204c045a35a00e7
ilikemen6969hitmeup-cell <ilikemen6969hitmeup@gmail.com>
2026-09-16 18:59:14 -0500

    Change server port to 10000
    Updated server to use port 10000 instead of 3000.

--- server.js
+++ server.js
@@ -49,8 +49,7 @@ server.on('upgrade', (req, socket, head) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 10000;
 server.listen({ port: PORT, host: '0.0.0.0' }, () => {
   console.log(`Server live on port ${PORT}`);
 });
